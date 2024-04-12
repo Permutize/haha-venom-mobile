bd5aed21db31a4852f9abcc9cde0fd54ff19442e
Dat Tran <nartjie11092@gmail.com>
2024-04-12 17:28:14 +0700

    Init project

---
+++ App.tsx
@@ -0,0 +1,161 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useCallback} from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import {authorize, AuthorizeResult} from 'react-native-app-auth';
+import Config from 'react-native-config';
+import axios from 'axios';
+
+const X_CONFIG = {
+  issuer: 'https://twitter.com/i/oauth2',
+  clientId: Config.X_CLIENT_ID ?? '',
+  redirectUrl: 'mau://connecttox',
+  scopes: ['users.read', 'tweet.read', 'follows.read', 'offline.access'],
+  serviceConfiguration: {
+    authorizationEndpoint: 'https://twitter.com/i/oauth2/authorize',
+    tokenEndpoint: 'https://api.twitter.com/2/oauth2/token',
+    revocationEndpoint: 'https://api.twitter.com/2/oauth2/revoke',
+  },
+};
+
+const getTwitterProfile = (auth: AuthorizeResult) => {
+  return axios({
+    method: 'GET',
+    headers: {Authorization: `Bearer ${auth.accessToken}`},
+    url: 'https://api.twitter.com/2/users/me',
+    params: {
+      'user.fields':
+        'created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld',
+    },
+  });
+};
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const connectToX = useCallback(async () => {
+    try {
+      // Connect to X
+      const result = await authorize(X_CONFIG);
+
+      // Get user profile
+      const profileRes = await getTwitterProfile(result);
+
+      // Save x auth locally
+      console.log(profileRes);
+    } catch (_error: any) {
+      console.log('connectToX', _error);
+    } finally {
+    }
+  }, []);
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
